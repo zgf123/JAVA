62d9a5607f4845f0e353f8b48b9e1a352c670cf8
zhangguangfei <zhangguangfei@bizseer.com>
2023-02-18 19:20:31 +0800

    java

---
+++ ChangeChar.java
@@ -0,0 +1,14 @@
+
+public class ChangeChar {
+  public static void main(String[] args) {
+    System.out.println("北京\t上海\t广州");
+    System.out.println("你好\n世界");
+    System.out.println("hello\\world");
+    System.out.println("C:\\usr\\locale\\bin");
+    System.out.println("C:\\\\usr\\\\locale\\\\bin");
+    System.out.println("老师说: \"今天体育课不上了\"");
+    System.out.println("老师说: \'今天体育课不上了\'");
+    System.out.println("我被替换了啊: \r世界");
+    System.out.println("你好呀: \r\n世界");
+  }
+}
---
+++ Hello.java
@@ -0,0 +1,7 @@
+// 这是java的快速入门，演示java的开发步骤
+public class Hello {
+  // 编写一个main方法
+  public static void main(String[] args) {
+    System.out.println("hello world");
+  }
+}
