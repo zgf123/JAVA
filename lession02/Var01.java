c0fad74f43503bd4851875cd39a0cb6b985eb15a
zhangguangfei <zhangguangfei@bizseer.com>
2023-02-18 21:15:54 +0800

    var

---
+++ lession02/Var01.java
@@ -0,0 +1,15 @@
+public class Var01 {
+
+  public static void main(String[] args) {
+    int age = 30;
+    double score = 88.9;
+    char gender = '男';
+    String name = "King";
+
+    System.out.println("人的信息如下");
+    System.out.println(age);
+    System.out.println(score);
+    System.out.println(gender);
+    System.out.println(name);
+  }
+}
