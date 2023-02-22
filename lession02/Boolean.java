127cf0af497c20af2baab037b6a7c40a69609d7d
zhangguangfei <zhangguangfei@bizseer.com>
2023-02-22 22:27:28 +0800

    convert

---
+++ lession02/Boolean.java
@@ -0,0 +1,12 @@
+/**
+ * Boolean
+ */
+public class Boolean {
+
+  public static void main(String[] args) {
+    boolean isPass = true;
+    if (isPass) {
+      System.out.println("考试通过");
+    }
+  }
+}
